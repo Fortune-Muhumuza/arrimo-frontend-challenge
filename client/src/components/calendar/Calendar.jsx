8680c669296d38f10699ace2834e2c916128783f
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2023-01-16 01:23:23 +0300

    feat: add add event functionality

--- client/src/components/calendar/Calendar.jsx
+++ client/src/components/calendar/Calendar.jsx
@@ -1,20 +1,92 @@
-import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin, { Draggable } from "@fullcalendar/interaction";
+import { Layout } from "antd";
 import "./Calendar.css";
-
-const events = [{ title: "Meeting", start: new Date() }];
+import { useState } from "react";
+import { Modal, Form, Input, Button } from "antd";
 
 function UserCalendar() {
+  const [selectedDates, setSelectedDates] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [eventTitle, setEventTitle] = useState("");
+  const [events, setEvents] = useState([
+    { title: "Meeting", start: new Date() },
+  ]);
+
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  const handleSelect = (info) => {
+    console.log("info", info);
+    setSelectedDates([info.startStr, info.endStr]);
+    toggleModal();
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const event = {
+      title: e.target.eventTitle.value,
+      start: selectedDates[0],
+      end: selectedDates[1],
+    };
+    setEvents([...events, event]);
+  };
+
+  const handleAddEvent = (e) => {
+    e.preventDefault();
+    const event = {
+      title: eventTitle,
+      start: selectedDates[0],
+      end: selectedDates[1],
+    };
+    console.log("event", event);
+    setEvents([...events, event]);
+    toggleModal();
+  };
+
   return (
-    <div>
+    <Layout>
       <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
-        weekends={false}
+        plugins={[dayGridPlugin, interactionPlugin]}
+        selectable={true}
+        select={handleSelect}
+        unselect={modalVisible ? null : () => setSelectedDates([])}
         events={events}
+        weekends={false}
         className="calendar-container"
       />
-    </div>
+      <Modal
+        title="Add Event"
+        open={modalVisible}
+        onCancel={() => {
+          toggleModal();
+          setSelectedDates([]);
+        }}
+        footer={null}
+      >
+        <Form>
+          <Form.Item label="Event Title">
+            <Input
+              placeholder="Event Title"
+              value={eventTitle}
+              onChange={(e) => setEventTitle(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item label="Start Date">
+            <Input value={selectedDates[0]} disabled />
+          </Form.Item>
+          <Form.Item label="End Date">
+            <Input value={selectedDates[1]} disabled />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" onClick={handleAddEvent}>
+              Add Event
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </Layout>
   );
 }
 
