efe965e04646fc4a3b37a6aefcbc9b22edaaa9b5
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2023-01-16 19:11:12 +0300

    fix: add show only selected menu item

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Menu } from "antd";
+import Users from "./users/Users";
+import UserCalendar from "./calendar/Calendar";
 
 const AppMenu = () => {
+  const [selectedMenuItem, setSelectedMenuItem] = useState("users");
+
   return (
-    <Menu mode="horizontal">
-      <Menu.Item key="users">Users</Menu.Item>
-      <Menu.Item key="calendar">Calendar</Menu.Item>
-    </Menu>
+    <>
+      <Menu
+        mode="horizontal"
+        onClick={({ key }) => setSelectedMenuItem(key)}
+        selectedKeys={[selectedMenuItem]}
+      >
+        <Menu.Item key="users">Users</Menu.Item>
+        <Menu.Item key="calendar">Calendar</Menu.Item>
+      </Menu>
+      {selectedMenuItem === "users" && <Users />}
+      {selectedMenuItem === "calendar" && <UserCalendar />}
+    </>
   );
 };
 
