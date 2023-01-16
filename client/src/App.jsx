b1980c337bd07ec8592f5ea63fdd9a708be6139f
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2023-01-17 01:45:45 +0300

    fix: fix auth page rendering issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,37 @@
 import "./App.css";
-import AppMenu from "./components/Menu";
 import { Layout } from "antd";
-import Users from "./components/users/Users";
-import UserCalendar from "./components/calendar/Calendar";
+import { useState } from "react";
+import AppMenu from "./components/Menu";
+import { Login, Signup } from "./components/auth/Auth";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentPage, setCurrentPage] = useState("home");
+
+  if (!isLoggedIn && currentPage !== "login" && currentPage !== "signup") {
+    setCurrentPage("login");
+  }
+
   return (
     <Layout>
-      <AppMenu />
-      <Users />
-      <UserCalendar />
+      <AppMenu
+        setCurrentPage={setCurrentPage}
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+      />
+      {isLoggedIn ? (
+        <>
+          {currentPage === "home" && <div>Home Page</div>}
+          {currentPage === "calendar" && <UserCalendar />}
+        </>
+      ) : (
+        currentPage === "login" && (
+          <Login
+            setIsLoggedIn={setIsLoggedIn}
+            setCurrentPage={setCurrentPage}
+          />
+        )
+      )}
     </Layout>
   );
 }
